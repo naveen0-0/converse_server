8fc6ea235e5ec26d541908c4cffa8a6344d9b878
naveen0-0 <baddigamnaveenkumarreddy1910@gmail.com>
2022-09-03 15:01:05 +0530

    Updated the Client Url

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
+const cookieParser = require('cookie-parser')
+const socketio = require('socket.io')
+const mongoose = require('mongoose')
 const express = require('express')
+const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const http = require('http')
-const socketio = require('socket.io')
-const cookieParser = require('cookie-parser')
+
 const apiRoutes = require('./routes/api')
 const authRoutes = require('./routes/auth')
-const dotenv = require('dotenv')
 const { socketIoLogic } = require('./socket/socket_io_logic')
 
-//@ App Initialization
+
 const app = express()
 
-//@ Server Intializing
 const server = http.createServer(app)
+const clientUrl = {
+  DEV:"http://localhost:3000",
+  PROD:"https://converse-1910.netlify.app"
+}
 
-//@ Socket IO Intialization
-const clientURL =  "https://converse-1910.netlify.app"
 const io = new socketio.Server(server,{
   cors: {
-     origin: clientURL,
+     origin: clientUrl.PROD,
      methods: ["GET", "POST"]
   }
 })
 
-//@ Middleware
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(helmet())
@@ -35,21 +37,18 @@ app.use(cors())
 app.use(cookieParser())
 dotenv.config()
 
-//@ Routes
 
 app.get('/', (req,res) => res.send({ message : "Welcome to the converse_server" }))
 app.use('/auth',authRoutes)
 app.use('/api',apiRoutes)
 
-//@ MongoDB Initialization
-mongoose.connect(process.env.MONGO)
+const MONGO = process.env.MONGO || "mongodb://localhost/chat"
+mongoose.connect(MONGO)
         .then(() => console.log("Mongo DB Connection Successful"))
         .catch(() => console.log("Mongo DB Connection Failure"))
 
-        
-//@ Server Io Logic
+
 socketIoLogic(io)
 
 
-//@ Server listening port
 server.listen(process.env.PORT, () => console.log(`Server listening on port ${process.env.PORT}`))
